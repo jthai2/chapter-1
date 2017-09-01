6d8aed831e4f591a1ce10985ee90662ffb2f158f
jthai2 <jimmythai35@gmail.com>
2017-09-01 08:30:02 -0500

    update fbhd

--- FixDebugOne1.java
+++
@@ -1,8 +0,0 @@
-public class FixDebugOne1
-{
-   /* This program displays a greeting */
-   public static void main(String[] arg)
-   {
-      System.out.println("Hello")
-   }
-}
--- FixDebugOne4.java
+++
@@ -1,8 +0,0 @@
-
-public class FixDebugOne4
-{
-  public static void main(string[] args)
-  {
-     J0ptionPane.showMessageDialog(null, "First GUI program");
-  }
-}
